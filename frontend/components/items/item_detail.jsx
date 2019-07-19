0bb39019013c8e1a624b04ccbe2422a8080ec8f4
Rupa Dhillon <rupa211984@gmail.com>
2019-07-18 22:02:55 -0700

    Show dummy item if user goes to bad item link

--- frontend/components/items/item_detail.jsx
+++ frontend/components/items/item_detail.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 const ItemDetail = ({ item, errors, deleteItem, history }) => {
+  item = item || { name: 'Bad Link: Item Does Not Exist', happiness: 0, price: 1000000, pokemon_id: -1 };
+
   const displayErrors = () => {
     return errors.join(' | ');
   };
@@ -11,10 +13,20 @@ const ItemDetail = ({ item, errors, deleteItem, history }) => {
     deleteItem(item.id);
   };
 
+  const trash = () => {
+    if (item.pokemon_id > -1) {
+      return (
+        <button className="delete-item" onClick={destroyItem}>
+          <i className="fas fa-trash-alt"></i>
+        </button>
+      )
+    }
+  };
+
   return (
     <div className="item-detail">
       <h2>{item.name}</h2>
-      <button className="delete-item" onClick={destroyItem}><i className="fas fa-trash-alt"></i></button>
+      {trash()}
       {displayErrors()}
       <p>Happiness: {item.happiness}</p>
       <p>Price: ${item.price}</p>
