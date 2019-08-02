bacf0e77a5949e3a0d8421a17873e0ca363d75fd
Rupa Dhillon <rupa211984@gmail.com>
2019-08-02 15:57:52 -0700

    Move logger to dev only

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
-import logger from 'redux-logger';
 import thunk from '../middleware/thunk';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 const configureStore = () => {
-  return createStore(rootReducer, applyMiddleware(thunk, logger));
+  return createStore(rootReducer, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
